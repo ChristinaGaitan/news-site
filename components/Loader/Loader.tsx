bcbd5a40151c0926d3f2586be90d7dee3e77e759
Christina Gaitan <christina.gaitan@gmail.com>
2025-01-16 11:27:04 -0600

    create Loader and PostBody components

---
+++ components/Loader/Loader.tsx
@@ -0,0 +1,5 @@
+import { Container } from "../Footer/style";
+
+export const Loader = () => {
+  return <Container>Loading...</Container>;
+};
---
+++ components/Post/PostBody.tsx
@@ -0,0 +1,23 @@
+import { Post } from "../../shared/types";
+import { Content, Figure, Meta, Title } from "./PostBodyStyle";
+
+type PostBodyProps = {
+  post: Post;
+};
+
+export const PostBody = ({ post }: PostBodyProps) => {
+  return (
+    <div>
+      <Title>{post.title}</Title>
+      <Figure>
+        <img src={post.image} alt={post.title} />
+      </Figure>
+      <Content dangerouslySetInnerHTML={{ __html: post.content }} />
+      <Meta>
+        <span>{post.date}</span>
+        <span>&middot;</span>
+        <a href={post.source}>Source</a>
+      </Meta>
+    </div>
+  );
+};
