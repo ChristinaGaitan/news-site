e8d08bbda677dc525d112b6e6ad4fa2859c90b02
Christina Gaitan <christina.gaitan@gmail.com>
2025-01-20 12:29:25 -0600

    modify Post page

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,33 +1,39 @@
 import { useRouter } from "next/router";
-import { Post as PostType } from "../../shared/types";
-import { GetStaticProps } from "next";
-import { postPaths as paths } from "../../shared/staticPaths";
+import { Post as PostType, Comment } from "../../shared/types";
+import { GetServerSideProps, GetStaticProps } from "next";
 import { fetchPost } from "../../api/post";
 import { Loader } from "../../components/Loader";
 import { PostBody } from "../../components/Post/PostBody";
+import { Comments } from "../../components/Comments";
+import { fetchComments } from "../../api/comments/fetch";
 
 type PostProps = {
   post: PostType;
+  comments: Comment[];
 };
 
-export const getStaticProps: GetStaticProps<PostProps> = async ({ params }) => {
+export const getServerSideProps: GetServerSideProps<PostProps> = async ({
+  params,
+}) => {
   if (typeof params.id !== "string") throw new Error("Unexpected id");
 
   const post = await fetchPost(params.id);
+  const comments = await fetchComments(params.id);
 
-  return { props: { post } };
+  return { props: { post, comments } };
 };
 
-export async function getStaticPaths() {
-  return { paths, fallback: true };
-}
-
-const Post = ({ post }: PostProps) => {
+const Post = ({ post, comments }: PostProps) => {
   const router = useRouter();
 
   if (router.isFallback) return <Loader />;
 
-  return <PostBody post={post} />;
+  return (
+    <>
+      <PostBody post={post} />
+      <Comments comments={comments} post={post.id} />
+    </>
+  );
 };
 
 export default Post;
