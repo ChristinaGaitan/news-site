9aa379b4b341f121bffb659971332ff514767386
Christina Gaitan <christina.gaitan@gmail.com>
2025-01-20 12:17:27 -0600

    fix Comment type in api

--- api/comments/fetch.ts
+++ api/comments/fetch.ts
@@ -1,5 +1,6 @@
 import { EntityId } from "../../shared/types";
 import { config } from "../config";
+import { Comment } from "../../shared/types";
 
 export async function fetchComments(postId: EntityId): Promise<Comment[]> {
   const res = await fetch(`${config.baseUrl}/comments/${postId}`);
